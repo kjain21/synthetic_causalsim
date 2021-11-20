1d1e5b8f2bbaa582f989bc2bf4aadf0a11c505dd
arashne <arashne@mit.edu>
2021-11-19 20:18:24 -0500

    synthtic_causalsim

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# synthetic_causalsim
+# Synthetic ABR Environment
+
+### Prerequisites
+python 3.7, numpy, scipy, cython, tqdm, python-wget
+
+### Environment interaction
+
+To learn how to interact with the environment, read the code in `sample_interact.py`
+
+### Trajectory generation
+
+To log trajectories for offline learning, first use these commands:
+
+```
+cd cpolicies
+make all
+cd ..
+```
+If the compilation is successful, use this command:
+```
+python3 run_exp.py
+```
+This command will likely take some time to execute (10-20 minutes). The outputs will also be large (~2GB).
+
+The result is 6 numpy arrays in `./output/`. Each include 2000 streaming sessions generated using one policy. Each session is 490 chunks in length.
+
+For each chunk, there is an array of length (19 + 1 + 1 + 19 + 1). In order, this array consists of:
+* An observation, with 19 dimensions
+  * Last 5 throughput measurements
+  * Last 5 chunk download times
+  * Buffer level
+  * Number of chunks left in video
+  * Previous action
+  * 6 file sizes for 6 possible encoding choices (the actions)
+* The chosen action
+* The reward in that state
+* The next observation, with 19 dimensions
+* Whether the session is over or not (always 1 at the end of the 490 chunks and 0 elsewhere)
---
+++ sample_interact.py
@@ -0,0 +1,47 @@
+import os
+import numpy as np
+from env.abr import ABRSimEnv
+from param import config
+from env.model import *
+
+
+def run_trajectories():
+    # Create output folder
+    os.makedirs(config.output_folder, exist_ok=True)
+
+    # Launch ABR environment
+    print('Setting up environment..')
+    env = ABRSimEnv()
+    # Use this command instead when you want to evaluate your policy
+    # env = ABRSimEnv(test_mode=True)
+
+    # Shorthand for number of actions
+    act_len = env.action_space.n
+
+    # Number of traces we intend to run through, more gives us a better evaluation
+    num_traces = 10
+
+    for _ in range(num_traces):
+        # Done in reset: Randomly choose a trace and starting point in it
+        obs, _ = env.reset()
+        done = False
+        t = 0
+
+        while not done:
+            # Choose an action through random policy
+            act = np.random.choice(act_len)
+
+            # Take the action
+            next_obs, rew, done, info = env.step(act)
+
+            # Print some statistics
+            print(f'At chunk {t}, the agent took action {act}, and got a reward {rew}')
+            print(f'\t\tThe observation was {obs}')
+
+            # Going forward: the next state at point t becomes the current state at point t+1
+            obs = next_obs
+            t += 1
+
+
+if __name__ == '__main__':
+    run_trajectories()
